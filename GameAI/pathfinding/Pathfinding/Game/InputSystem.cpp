88245369b9690b1a137abce4a679bc666ebcd0cc
Robert Gentile <robertngentile@gmail.com>
2016-11-08 17:23:29 -0500

    drawing the lettering in inputsystem, may move to gridvisualizer later

--- GameAI/pathfinding/Pathfinding/Game/InputSystem.cpp
+++ GameAI/pathfinding/Pathfinding/Game/InputSystem.cpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <sstream>
 #include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_ttf.h>
 
 
 InputSystem::InputSystem()
@@ -33,6 +34,7 @@ void InputSystem::cleanup()// easier to call rather than depending on destructor
 void InputSystem::update()
 {
 	static Vector2D lastPos(0.0f, 0.0f);
+	static Vector2D goalPos(0.0f, 0.0f);
 	al_get_mouse_state(&mMouse);
 	al_get_keyboard_state(&mKey);
 
@@ -51,15 +53,24 @@ void InputSystem::update()
 				GameMessage* pMessage = new PathToMessage(lastPos, pos);
 				mpMessageManager->addMessage(pMessage, 0);
 				firstSelect = false;
+				goalPos = Vector2D(mMouse.x, mMouse.y);
 			}
 		}
 
 	}
-	
+	// drawing start and end goal and letters
 	if (firstSelect)
 	{
 		Vector2D ulPos = gpGameA->getGrid()->getULCornerOfSquare(gpGameA->getGrid()->getSquareIndexFromPixelXY((int)lastPos.getX(), (int)lastPos.getY())); 
 		al_draw_filled_rectangle(ulPos.getX(), ulPos.getY(), ulPos.getX() + 32, ulPos.getY() + 32, al_map_rgb(1, 255, 128));
+		al_draw_text(gpGame->getFont(), al_map_rgb(0, 0, 0), ulPos.getX()+ 16, ulPos.getY(),ALLEGRO_ALIGN_CENTER, "S" );
+	}
+	else
+	{
+		Vector2D ulPos = gpGameA->getGrid()->getULCornerOfSquare(gpGameA->getGrid()->getSquareIndexFromPixelXY((int)lastPos.getX(), (int)lastPos.getY()));
+		al_draw_text(gpGame->getFont(), al_map_rgb(0, 0, 0), ulPos.getX() + 16, ulPos.getY(), ALLEGRO_ALIGN_CENTER, "S");
+		Vector2D ilPos = gpGameA->getGrid()->getULCornerOfSquare(gpGameA->getGrid()->getSquareIndexFromPixelXY((int)goalPos.getX(), (int)goalPos.getY()));
+		al_draw_text(gpGame->getFont(), al_map_rgb(0, 0, 0), ilPos.getX() + 16, ilPos.getY(), ALLEGRO_ALIGN_CENTER, "G");
 	}
 
 
